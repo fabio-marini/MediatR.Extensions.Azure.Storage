c8ea9114a25d0fff413cf9f021d65ad59c17ad18
Fabio Marini <f.marini@gmail.com>
2021-11-10 21:55:32 +0100

    Added initial integration tests for blob extensions

---
+++ MediatR.Extensions.Azure.Storage.Tests/Extensions/IntegrationTests.cs
@@ -0,0 +1,192 @@
+﻿using Azure.Storage.Blobs;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MediatR.Extensions.Azure.Storage.Tests.Extensions
+{
+    public class IntegrationTests
+    {
+        private readonly BlobContainerClient containerClient;
+
+        public IntegrationTests()
+        {
+            containerClient = new BlobContainerClient("UseDevelopmentStorage=true", "integration-tests");
+            containerClient.CreateIfNotExists();
+        }
+
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[] { TestCommand.Default, Unit.Value };
+            yield return new object[] { TestQuery.Default, TestResult.Default };
+        }
+
+        [Theory(DisplayName = "All BlobRequestBehaviors execute correctly"), MemberData(nameof(TestData))]
+        public async Task Test1<TRequest, TResponse>(TRequest req, TResponse res) where TRequest : IRequest<TResponse>
+        {
+            // TODO: assert more details in downloaded delegate!
+            var blobName = Guid.NewGuid().ToString() + ".json";
+
+            var opt = new BlobOptions<TRequest>
+            {
+                IsEnabled = true,
+                BlobClient = (req, ctx) => containerClient.GetBlobClient(blobName),
+                Downloaded = (res, req, ctx) =>
+                {
+                    res.Content.Should().NotBeNull().ToString();
+                    res.Content.Should().ToString().Length.Should().BeGreaterThan(0);
+
+                    return Task.CompletedTask;
+                }
+            };
+
+            var serviceProvider = new ServiceCollection()
+
+                .AddBlobExtensions(opt, new BlobOptions<TResponse>())
+                .BuildServiceProvider();
+
+            var uploadExtension = serviceProvider.GetRequiredService<UploadBlobRequestBehavior<TRequest, TResponse>>();
+            var downloadExtension = serviceProvider.GetRequiredService<DownloadBlobRequestBehavior<TRequest, TResponse>>();
+            var deleteExtension = serviceProvider.GetRequiredService<DeleteBlobRequestBehavior<TRequest, TResponse>>();
+
+            opt.BlobClient(req, null).Exists().Value.Should().BeFalse();
+
+            _ = await uploadExtension.Handle(req, CancellationToken.None, () => Task.FromResult(res));
+
+            opt.BlobClient(req, null).Exists().Value.Should().BeTrue();
+
+            _ = await downloadExtension.Handle(req, CancellationToken.None, () => Task.FromResult(res));
+
+            _ = await deleteExtension.Handle(req, CancellationToken.None, () => Task.FromResult(res));
+
+            opt.BlobClient(req, null).Exists().Value.Should().BeFalse();
+        }
+
+        [Theory(DisplayName = "All BlobResponseBehaviors execute correctly"), MemberData(nameof(TestData))]
+        public async Task Test2<TRequest, TResponse>(TRequest req, TResponse res) where TRequest : IRequest<TResponse>
+        {
+            // TODO: assert more details in downloaded delegate!
+            var blobName = Guid.NewGuid().ToString() + ".json";
+
+            var opt = new BlobOptions<TResponse>
+            {
+                IsEnabled = true,
+                BlobClient = (req, ctx) => containerClient.GetBlobClient(blobName),
+                Downloaded = (res, req, ctx) =>
+                {
+                    res.Content.Should().NotBeNull();
+                    res.Content.Should().ToString().Length.Should().BeGreaterThan(0);
+
+                    return Task.CompletedTask;
+                }
+            };
+
+            var serviceProvider = new ServiceCollection()
+
+                .AddBlobExtensions(new BlobOptions<TRequest>(), opt)
+                .BuildServiceProvider();
+
+            var uploadExtension = serviceProvider.GetRequiredService<UploadBlobResponseBehavior<TRequest, TResponse>>();
+            var downloadExtension = serviceProvider.GetRequiredService<DownloadBlobResponseBehavior<TRequest, TResponse>>();
+            var deleteExtension = serviceProvider.GetRequiredService<DeleteBlobResponseBehavior<TRequest, TResponse>>();
+
+            opt.BlobClient(res, null).Exists().Value.Should().BeFalse();
+
+            _ = await uploadExtension.Handle(req, CancellationToken.None, () => Task.FromResult(res));
+
+            opt.BlobClient(res, null).Exists().Value.Should().BeTrue();
+
+            _ = await downloadExtension.Handle(req, CancellationToken.None, () => Task.FromResult(res));
+
+            _ = await deleteExtension.Handle(req, CancellationToken.None, () => Task.FromResult(res));
+
+            opt.BlobClient(res, null).Exists().Value.Should().BeFalse();
+        }
+
+        [Theory(DisplayName = "All BlobRequestProcessors execute correctly"), MemberData(nameof(TestData))]
+        public async Task Test3<TRequest, TResponse>(TRequest req, TResponse res) where TRequest : IRequest<TResponse>
+        {
+            // TODO: assert more details in downloaded delegate!
+            var blobName = Guid.NewGuid().ToString() + ".json";
+
+            var opt = new BlobOptions<TRequest>
+            {
+                IsEnabled = true,
+                BlobClient = (req, ctx) => containerClient.GetBlobClient(blobName),
+                Downloaded = (res, req, ctx) =>
+                {
+                    res.Content.Should().NotBeNull().ToString();
+                    res.Content.Should().ToString().Length.Should().BeGreaterThan(0);
+
+                    return Task.CompletedTask;
+                }
+            };
+
+            var serviceProvider = new ServiceCollection()
+
+                .AddBlobExtensions(opt, new BlobOptions<TResponse>())
+                .BuildServiceProvider();
+
+            var uploadExtension = serviceProvider.GetRequiredService<UploadBlobRequestProcessor<TRequest>>();
+            var downloadExtension = serviceProvider.GetRequiredService<DownloadBlobRequestProcessor<TRequest>>();
+            var deleteExtension = serviceProvider.GetRequiredService<DeleteBlobRequestProcessor<TRequest>>();
+
+            opt.BlobClient(req, null).Exists().Value.Should().BeFalse();
+
+            await uploadExtension.Process(req, CancellationToken.None);
+
+            opt.BlobClient(req, null).Exists().Value.Should().BeTrue();
+
+            await downloadExtension.Process(req, CancellationToken.None);
+
+            await deleteExtension.Process(req, CancellationToken.None);
+
+            opt.BlobClient(req, null).Exists().Value.Should().BeFalse();
+        }
+
+        [Theory(DisplayName = "All BlobResponseProcessors execute correctly"), MemberData(nameof(TestData))]
+        public async Task Test4<TRequest, TResponse>(TRequest req, TResponse res) where TRequest : IRequest<TResponse>
+        {
+            // TODO: assert more details in downloaded delegate!
+            var blobName = Guid.NewGuid().ToString() + ".json";
+
+            var opt = new BlobOptions<TResponse>
+            {
+                IsEnabled = true,
+                BlobClient = (req, ctx) => containerClient.GetBlobClient(blobName),
+                Downloaded = (res, req, ctx) =>
+                {
+                    res.Content.Should().NotBeNull();
+                    res.Content.Should().ToString().Length.Should().BeGreaterThan(0);
+
+                    return Task.CompletedTask;
+                }
+            };
+
+            var serviceProvider = new ServiceCollection()
+
+                .AddBlobExtensions(new BlobOptions<TRequest>(), opt)
+                .BuildServiceProvider();
+
+            var uploadExtension = serviceProvider.GetRequiredService<UploadBlobResponseProcessor<TRequest, TResponse>>();
+            var downloadExtension = serviceProvider.GetRequiredService<DownloadBlobResponseProcessor<TRequest, TResponse>>();
+            var deleteExtension = serviceProvider.GetRequiredService<DeleteBlobResponseProcessor<TRequest, TResponse>>();
+
+            opt.BlobClient(res, null).Exists().Value.Should().BeFalse();
+
+            await uploadExtension.Process(req, res, CancellationToken.None);
+
+            opt.BlobClient(res, null).Exists().Value.Should().BeTrue();
+
+            await downloadExtension.Process(req, res, CancellationToken.None);
+
+            await deleteExtension.Process(req, res, CancellationToken.None);
+
+            opt.BlobClient(res, null).Exists().Value.Should().BeFalse();
+        }
+    }
+}
--- MediatR.Extensions.Azure.Storage.Tests/ServiceCollectionExtensions.cs
+++ MediatR.Extensions.Azure.Storage.Tests/ServiceCollectionExtensions.cs
@@ -37,6 +37,38 @@ namespace MediatR.Extensions.Azure.Storage.Tests
                 ;
         }
 
+        public static IServiceCollection AddQueueExtensions<TRequest, TResponse>(this IServiceCollection services) where TRequest : IRequest<TResponse>
+        {
+            return services
+
+                .AddOptions<QueueOptions<TRequest>>().Services
+                .AddOptions<QueueOptions<TResponse>>().Services
+
+                .AddSingleton<Mock<SendMessageCommand<TRequest>>>(sp =>
+                {
+                    var opt = sp.GetRequiredService<IOptions<QueueOptions<TRequest>>>();
+
+                    return new Mock<SendMessageCommand<TRequest>>(opt, null, null);
+                })
+                .AddSingleton<Mock<SendMessageCommand<TResponse>>>(sp =>
+                {
+                    var opt = sp.GetRequiredService<IOptions<QueueOptions<TResponse>>>();
+
+                    return new Mock<SendMessageCommand<TResponse>>(opt, null, null);
+                })
+
+                .AddTransient<SendMessageCommand<TRequest>>(sp => sp.GetRequiredService<Mock<SendMessageCommand<TRequest>>>().Object)
+                .AddTransient<SendMessageCommand<TResponse>>(sp => sp.GetRequiredService<Mock<SendMessageCommand<TResponse>>>().Object)
+
+                .AddTransient<SendMessageRequestBehavior<TRequest, TResponse>>()
+                .AddTransient<SendMessageResponseBehavior<TRequest, TResponse>>()
+                .AddTransient<SendMessageRequestProcessor<TRequest>>()
+                .AddTransient<SendMessageResponseProcessor<TRequest, TResponse>>()
+
+                ;
+        }
+
+        // options don't matter, commands are mocks
         public static IServiceCollection AddBlobExtensions<TRequest, TResponse>(this IServiceCollection services) where TRequest : IRequest<TResponse>
         {
             return services
@@ -68,34 +100,37 @@ namespace MediatR.Extensions.Azure.Storage.Tests
                 ;
         }
 
-        public static IServiceCollection AddQueueExtensions<TRequest, TResponse>(this IServiceCollection services) where TRequest : IRequest<TResponse>
+        // options are required, commands are real instances
+        public static IServiceCollection AddBlobExtensions<TRequest, TResponse>(this IServiceCollection services, BlobOptions<TRequest> req, BlobOptions<TResponse> res) where TRequest : IRequest<TResponse>
         {
             return services
 
-                .AddOptions<QueueOptions<TRequest>>().Services
-                .AddOptions<QueueOptions<TResponse>>().Services
+                .AddTransient<IOptions<BlobOptions<TRequest>>>(sp => Options.Create(req))
+                .AddTransient<UploadBlobCommand<TRequest>>()
+                .AddTransient<DownloadBlobCommand<TRequest>>()
+                .AddTransient<DeleteBlobCommand<TRequest>>()
 
-                .AddSingleton<Mock<SendMessageCommand<TRequest>>>(sp =>
-                {
-                    var opt = sp.GetRequiredService<IOptions<QueueOptions<TRequest>>>();
+                .AddTransient<IOptions<BlobOptions<TResponse>>>(sp => Options.Create(res))
+                .AddTransient<UploadBlobCommand<TResponse>>()
+                .AddTransient<DownloadBlobCommand<TResponse>>()
+                .AddTransient<DeleteBlobCommand<TResponse>>()
 
-                    return new Mock<SendMessageCommand<TRequest>>(opt, null, null);
-                })
-                .AddSingleton<Mock<SendMessageCommand<TResponse>>>(sp =>
-                {
-                    var opt = sp.GetRequiredService<IOptions<QueueOptions<TResponse>>>();
+                .AddTransient<UploadBlobRequestBehavior<TRequest, TResponse>>()
+                .AddTransient<DownloadBlobRequestBehavior<TRequest, TResponse>>()
+                .AddTransient<DeleteBlobRequestBehavior<TRequest, TResponse>>()
 
-                    return new Mock<SendMessageCommand<TResponse>>(opt, null, null);
-                })
+                .AddTransient<UploadBlobResponseBehavior<TRequest, TResponse>>()
+                .AddTransient<DownloadBlobResponseBehavior<TRequest, TResponse>>()
+                .AddTransient<DeleteBlobResponseBehavior<TRequest, TResponse>>()
 
-                .AddTransient<SendMessageCommand<TRequest>>(sp => sp.GetRequiredService<Mock<SendMessageCommand<TRequest>>>().Object)
-                .AddTransient<SendMessageCommand<TResponse>>(sp => sp.GetRequiredService<Mock<SendMessageCommand<TResponse>>>().Object)
+                .AddTransient<UploadBlobRequestProcessor<TRequest>>()
+                .AddTransient<DownloadBlobRequestProcessor<TRequest>>()
+                .AddTransient<DeleteBlobRequestProcessor<TRequest>>()
 
-                .AddTransient<SendMessageRequestBehavior<TRequest, TResponse>>()
-                .AddTransient<SendMessageResponseBehavior<TRequest, TResponse>>()
-                .AddTransient<SendMessageRequestProcessor<TRequest>>()
-                .AddTransient<SendMessageResponseProcessor<TRequest, TResponse>>()
-
+                .AddTransient<UploadBlobResponseProcessor<TRequest, TResponse>>()
+                .AddTransient<DownloadBlobResponseProcessor<TRequest, TResponse>>()
+                .AddTransient<DeleteBlobResponseProcessor<TRequest, TResponse>>()
+ 
                 ;
         }
     }
